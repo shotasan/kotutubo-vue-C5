95119d2358399a76fc19485b390541da1499a685
shotasan <wondermoai@gmail.com>
2020-01-10 01:08:15 +0900

    Fix Q3

--- sample.py
+++ sample.py
@@ -1,25 +1,45 @@
-def length():
-    return list(range(1, 5))
+# 列数
+def column():
+    return list(range(1, 10))
+
+# 列数を等倍したリストを作成し、先頭行と一列目を追加して出力
 
 
-def cal():
-    result = []
-    map(lambda n: result.append(double(n)), length())
-    map(lambda m: m.insert(0, m[0]), result)
-    top(result)
-    return result
+def main():
+    base_list = []
+    for i in column():
+        base_list.append(multiple(i))
+    add_top_line(base_list)
+    result = convert_digits(base_list)
+    for res in result:
+        print(*res)
+
+# カラムを等倍し、先頭列を追加
 
 
-def double(n):
-    return map(lambda m: n * m, length())
+def multiple(i):
+    tmp = []
+    for j in column():
+        tmp.append(i * j)
+    tmp.insert(0, i)
+    return tmp
+
+# 一行目のリストを追加する
 
 
-def top(result):
-    top_line = length()
-    top_line.insert(0, ' ')
-    result.insert(0, top_line)
+def add_top_line(base_list):
+    col = column()
+    col.insert(0, ' ')
+    base_list.insert(0, col)
+    return base_list
+
+# 桁数を揃える
 
 
-# print(cal())
-for name in cal():
-    print(name.join)
+def convert_digits(base_list):
+    base_list = map(lambda lists: [str(lis).rjust(2)
+                                   for lis in lists], base_list)
+    return list(base_list)
+
+
+main()
